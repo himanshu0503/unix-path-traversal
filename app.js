ba3751c4c7e9423217617cf370ebe153716f50f8
himanshu0503 <hemanshu0503@gmail.com>
2018-09-07 16:46:02 +0530

    Adds app.js to support operations

---
+++ app.js
@@ -0,0 +1,31 @@
+const Node = require('./classes/node.js');
+const Utils = require('./utils/utilsExporter.js');
+const prompt = require('prompt');
+prompt.start();
+
+let newNode = new Node('root', null);
+let rootNode = newNode;
+let context = {
+  startNode: newNode
+};
+
+function main() {
+  prompt.get(['input'], function(err, result) {
+    var args = result.input.split(' ');
+    let cmd = args[0];
+    let values = args.splice(1, args.length);
+    if (cmd === 'quit') {
+      console.log('EXITING APPLICATION');
+    } else if (typeof Utils[cmd] === 'function') {
+      let cmdResult = Utils[cmd](rootNode, context, values);
+      console.log(cmdResult);
+      main();
+    } else {
+      console.log('ERR: CANNOT RECOGNIZE INPUT.');
+      console.log('Please type help to get the list of supported commands')
+      main();
+    }
+  });
+}
+
+main();
