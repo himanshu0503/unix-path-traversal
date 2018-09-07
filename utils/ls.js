13acf05cc4f604cd3f5f6e9d5c174ed283c5fe7e
himanshu0503 <hemanshu0503@gmail.com>
2018-09-07 16:58:33 +0530

    Adds todo for ls

--- utils/ls.js
+++ utils/ls.js
@@ -1,5 +1,5 @@
 module.exports = ls;
-
+//TODO: Support ls of multiple directories
 function ls(rootNode, context) {
   let childrens = context.startNode.getAllChildren();
   return childrens.join('\n');
